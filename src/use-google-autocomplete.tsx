a1430fde6014ea93f690636a35399ba1d8df2788
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2019-09-17 03:29:41 +0600

    updated CORS server

--- src/use-google-autocomplete.tsx
+++ src/use-google-autocomplete.tsx
@@ -12,10 +12,7 @@ const initialState = {
   error: null,
 }
 
-const cors =
-  process.env.NODE_ENV !== 'production'
-    ? 'https://cors-anywhere.herokuapp.com/'
-    : ''
+const cors = 'https://cors.theplayerfinder.com/'
 
 export default function useGoogleAutocomplete({
   apiKey,
